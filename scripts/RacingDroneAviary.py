d0fc62341c001bd693af11bc8baf811f013b00e3
mohil96 <mohilshah96@gmail.com>
2022-11-30 02:04:11 -0800

    intergrated reward function

--- scripts/RacingDroneAviary.py
+++ scripts/RacingDroneAviary.py
@@ -7,27 +7,29 @@ from scipy.spatial.transform import Rotation as R
 
 from gym_pybullet_drones.utils.enums import DroneModel, Physics
 from gym_pybullet_drones.envs.single_agent_rl.BaseSingleAgentAviary import ActionType, ObservationType, BaseSingleAgentAviary
+from reward_function import final_reward
+
 
 class RacingDroneAviary(BaseSingleAgentAviary):
     """Single agent RL problem: fly through a gate."""
-    
+
     ################################################################################
-    
+
     def __init__(self,
-                 drone_model: DroneModel=DroneModel.CF2X,
+                 drone_model: DroneModel = DroneModel.CF2X,
                  initial_xyzs=None,
                  initial_rpys=None,
-                 physics: Physics=Physics.PYB,
-                 freq: int=240,
-                 aggregate_phy_steps: int=1,
+                 physics: Physics = Physics.PYB,
+                 freq: int = 240,
+                 aggregate_phy_steps: int = 1,
                  gui=False,
-                 record=False, 
-                 obs: ObservationType=ObservationType.KIN,
-                 act: ActionType=ActionType.RPM,
+                 record=False,
+                 obs: ObservationType = ObservationType.KIN,
+                 act: ActionType = ActionType.RPM,
                  obstaclesCenterPosition=None,
                  obstaclesOrientation=None,
-                 obstaclesStd: float=0.001,
-                 gate_width: float=1.25,
+                 obstaclesStd: float = 0.001,
+                 gate_width: float = 1.25,
                  ):
         """Initialization of a single agent RL environment.
 
@@ -78,7 +80,7 @@ class RacingDroneAviary(BaseSingleAgentAviary):
         self.current_obstable_index = 0
 
     ################################################################################
-        
+
     def _addObstacles(self):
         """Add obstacles to the environment.
 
@@ -89,18 +91,19 @@ class RacingDroneAviary(BaseSingleAgentAviary):
         n = len(self.obstaclesCenterPosition)
 
         for i in range(n):
-            center = [random.gauss(self.obstaclesCenterPosition[i][0], self.obstaclesStd), 
-                    random.gauss(self.obstaclesCenterPosition[i][1], self.obstaclesStd), 
-                    random.gauss(self.obstaclesCenterPosition[i][2], self.obstaclesStd)]
+            center = [random.gauss(self.obstaclesCenterPosition[i][0], self.obstaclesStd),
+                      random.gauss(self.obstaclesCenterPosition[i][1], self.obstaclesStd),
+                      random.gauss(self.obstaclesCenterPosition[i][2], self.obstaclesStd)]
 
-            if center[2] < self.gate_width/2: center[2] = self.gate_width/2
+            if center[2] < self.gate_width / 2:
+                center[2] = self.gate_width / 2
 
             obstacleID = p.loadURDF(pkg_resources.resource_filename('gym_pybullet_drones', "assets/gates.urdf"),
-                                        center,
-                                        p.getQuaternionFromEuler(self.obstaclesOrientation[i]),
-                                        useFixedBase=1,
-                                        physicsClientId=self.CLIENT
-                        )
+                                    center,
+                                    p.getQuaternionFromEuler(self.obstaclesOrientation[i]),
+                                    useFixedBase=1,
+                                    physicsClientId=self.CLIENT
+                                    )
             self.obstacleIDs.append(obstacleID)
 
     def _getGatesStateVector(self, nth_drone, gateIDs):
@@ -113,13 +116,13 @@ class RacingDroneAviary(BaseSingleAgentAviary):
 
         Returns
             [o_1, alpha_1, o_2, alpha_2]
-            sperical coordinate o_1, o_2 = (p_r, p_theta, p_phi), 
+            sperical coordinate o_1, o_2 = (p_r, p_theta, p_phi),
             alpha_1, alpha_2 = angle between gate normal and the vector pointing from the quadrotor to the center of gate i
         """
         # Get the drone pose
         drone_state = self._getDroneStateVector(nth_drone)
-        drone_center = drone_state[0,3]
-        drone_rotation_matrix = R.from_quat(drone_state[3,7]).as_matrix()
+        drone_center = drone_state[0, 3]
+        drone_rotation_matrix = R.from_quat(drone_state[3, 7]).as_matrix()
 
         # Get the gate pose
         currentGateID = gateIDs[0]
@@ -128,44 +131,44 @@ class RacingDroneAviary(BaseSingleAgentAviary):
         current_gate_rotation_matrix = R.from_quat(currentGateOri).as_matrix()
         if nextGateID == -999:
             next_gate_rotation_matrix = current_gate_rotation_matrix
-            nextGatePos = currentGatePos + current_gate_rotation_matrix @ np.array([[0],[1],[0]])
+            nextGatePos = currentGatePos + current_gate_rotation_matrix @ np.array([[0], [1], [0]])
         else:
             nextGatePos, nextGateOri = p.getBasePositionAndOrientation(objectUniqueId=nextGateID)
             next_gate_rotation_matrix = R.from_quat(nextGateOri).as_matrix()
 
-        #### consider there is a vector p from plane coordinate system to gate center,
+        # consider there is a vector p from plane coordinate system to gate center,
         p = np.array(currentGatePos) - np.array(drone_center)
 
         # get p_r: The distance between drone center and gate center
         p_r = np.linalg.norm(p)
 
-        # get p_theta: 
-        p_along_z_axis = np.dot(drone_rotation_matrix[:,2], p) * drone_rotation_matrix[:,2]
+        # get p_theta:
+        p_along_z_axis = np.dot(drone_rotation_matrix[:, 2], p) * drone_rotation_matrix[:, 2]
         p_along_xy_plane = p - p_along_z_axis
-        p_theta = np.arccos(np.dot(p_along_xy_plane, p)/np.linalg.norm(p)/np.linalg.norm(p_along_xy_plane))
+        p_theta = np.arccos(np.dot(p_along_xy_plane, p) / np.linalg.norm(p) / np.linalg.norm(p_along_xy_plane))
 
-        # get p_phi: 
-        p_phi = np.arccos(np.dot(drone_rotation_matrix[:,2], p)/np.linalg.norm(p))
+        # get p_phi:
+        p_phi = np.arccos(np.dot(drone_rotation_matrix[:, 2], p) / np.linalg.norm(p))
 
         # get alpha_1: Note that the y axis of gate is the normal of the gate
-        alpha_1 = np.arccos(current_gate_rotation_matrix[:,1], p)/np.linalg.norm(p)
+        alpha_1 = np.arccos(current_gate_rotation_matrix[:, 1], p) / np.linalg.norm(p)
 
-        #### consider there is a vector q from first gate coordinate system to second gate center,
+        # consider there is a vector q from first gate coordinate system to second gate center,
         q = np.array(nextGatePos) - np.array(currentGatePos)
 
         # get p_r: The distance between drone center and gate center
         q_r = np.linalg.norm(q)
 
-        # get q_theta: 
-        q_along_z_axis = np.dot(current_gate_rotation_matrix[:,2], q) * current_gate_rotation_matrix[:,2]
+        # get q_theta:
+        q_along_z_axis = np.dot(current_gate_rotation_matrix[:, 2], q) * current_gate_rotation_matrix[:, 2]
         q_along_xy_plane = q - q_along_z_axis
-        q_theta = np.arccos(np.dot(q_along_xy_plane, q)/np.linalg.norm(q)/np.linalg.norm(q_along_xy_plane))
+        q_theta = np.arccos(np.dot(q_along_xy_plane, q) / np.linalg.norm(q) / np.linalg.norm(q_along_xy_plane))
 
-        # get q_phi: 
-        q_phi = np.arccos(np.dot(current_gate_rotation_matrix[:,2], q)/np.linalg.norm(q))
+        # get q_phi:
+        q_phi = np.arccos(np.dot(current_gate_rotation_matrix[:, 2], q) / np.linalg.norm(q))
 
         # get alpha_2: Note that the y axis of gate is the normal of the gate
-        alpha_2 = np.arccos(next_gate_rotation_matrix[:,1], q)/np.linalg.norm(q)
+        alpha_2 = np.arccos(next_gate_rotation_matrix[:, 1], q) / np.linalg.norm(q)
 
         return np.array([p_r, p_theta, p_phi, alpha_1, q_r, q_theta, q_phi, alpha_2])
 
@@ -186,17 +189,17 @@ class RacingDroneAviary(BaseSingleAgentAviary):
         quad_state = np.hstack((self.vel[nth_drone, :], self.acc[nth_drone, :], rotation_matrix.flatten(), self.ang_v[nth_drone, :]))
 
         # Find the current gate and next gate
-        if self.current_obstable_index+1 < len(self.obstacleIDs):
-            gateIDs = [self.obstacleIDs[self.current_obstable_index], self.obstacleIDs[self.current_obstable_index+1]]
+        if self.current_obstable_index + 1 < len(self.obstacleIDs):
+            gateIDs = [self.obstacleIDs[self.current_obstable_index], self.obstacleIDs[self.current_obstable_index + 1]]
         else:
             gateIDs = [self.obstacleIDs[self.current_obstable_index], -999]
-        
+
         gate_state = self._getGatesStateVector(nth_drone, gateIDs)
 
         return np.hstack((quad_state, gate_state))
 
     ################################################################################
-    
+
     def _computeReward(self):
         """Computes the current reward value.
 
@@ -206,12 +209,27 @@ class RacingDroneAviary(BaseSingleAgentAviary):
             The reward.
 
         """
-        # TODO
-        # Add mohil's reward functions
+
+        g1 = self.obstaclesCenterPosition[0]  # gate1 centre
+        g2 = self.obstaclesCenterPosition[1]  # gate2 centre
+
+        gm1 = self.obstaclesOrientation[0]
+        gm2 = self.obstaclesOrientation[1]
+
+        wt = self.ang_v  # body rates
+
+        p = self.pos  # current position
+        p_prev = self.prev_pos  # previous position
+        wg = self.gate_width  # side length of the rectangular gate
+
+        dmax = 2  # specifies a threshold on the distance to the gate center in order to activate the safety reward
+        a = 2  # hyperparameter that trades off between progress maximization and risk minimization
+        b = -0.5  # weight for penalty body rate
+        return final_reward(p, p_prev, g1, g2, gm1, gm2, a, b, dmax, wt, wg)
         # return -10 * np.linalg.norm(np.array([0, -2*norm_ep_time, 0.75])-state[0:3])**2
 
     ################################################################################
-    
+
     def _computeDone(self):
         """Computes the current done value.
 
@@ -222,13 +240,13 @@ class RacingDroneAviary(BaseSingleAgentAviary):
 
         """
         # update current_obstable_index
-        if self.step_counter/self.SIM_FREQ > self.EPISODE_LEN_SEC:
+        if self.step_counter / self.SIM_FREQ > self.EPISODE_LEN_SEC:
             return True
         else:
             return False
 
     ################################################################################
-    
+
     def _computeInfo(self):
         """Computes the current info dict(s).
 
@@ -240,10 +258,10 @@ class RacingDroneAviary(BaseSingleAgentAviary):
             Dummy value.
 
         """
-        return {"answer": 42} #### Calculated by the Deep Thought supercomputer in 7.5M years
+        return {"answer": 42}  # Calculated by the Deep Thought supercomputer in 7.5M years
 
     ################################################################################
-    
+
     def _clipAndNormalizeState(self,
                                state
                                ):
@@ -260,13 +278,13 @@ class RacingDroneAviary(BaseSingleAgentAviary):
             (20,)-shaped array of floats containing the normalized state of a single drone.
 
         """
-        MAX_LIN_VEL_XY = 3 
+        MAX_LIN_VEL_XY = 3
         MAX_LIN_VEL_Z = 1
 
-        MAX_XY = MAX_LIN_VEL_XY*self.EPISODE_LEN_SEC
-        MAX_Z = MAX_LIN_VEL_Z*self.EPISODE_LEN_SEC
+        MAX_XY = MAX_LIN_VEL_XY * self.EPISODE_LEN_SEC
+        MAX_Z = MAX_LIN_VEL_Z * self.EPISODE_LEN_SEC
 
-        MAX_PITCH_ROLL = np.pi # Full range
+        MAX_PITCH_ROLL = np.pi  # Full range
 
         clipped_pos_xy = np.clip(state[0:2], -MAX_XY, MAX_XY)
         clipped_pos_z = np.clip(state[2], 0, MAX_Z)
@@ -286,10 +304,10 @@ class RacingDroneAviary(BaseSingleAgentAviary):
         normalized_pos_xy = clipped_pos_xy / MAX_XY
         normalized_pos_z = clipped_pos_z / MAX_Z
         normalized_rp = clipped_rp / MAX_PITCH_ROLL
-        normalized_y = state[9] / np.pi # No reason to clip
+        normalized_y = state[9] / np.pi  # No reason to clip
         normalized_vel_xy = clipped_vel_xy / MAX_LIN_VEL_XY
         normalized_vel_z = clipped_vel_z / MAX_LIN_VEL_XY
-        normalized_ang_vel = state[13:16]/np.linalg.norm(state[13:16]) if np.linalg.norm(state[13:16]) != 0 else state[13:16]
+        normalized_ang_vel = state[13:16] / np.linalg.norm(state[13:16]) if np.linalg.norm(state[13:16]) != 0 else state[13:16]
 
         norm_and_clipped = np.hstack([normalized_pos_xy,
                                       normalized_pos_z,
@@ -308,9 +326,9 @@ class RacingDroneAviary(BaseSingleAgentAviary):
         # TODO
         # Clip the track states
         pass
-    
+
     ################################################################################
-    
+
     def _clipAndNormalizeStateWarning(self,
                                       state,
                                       clipped_pos_xy,
@@ -322,7 +340,7 @@ class RacingDroneAviary(BaseSingleAgentAviary):
         """Debugging printouts associated to `_clipAndNormalizeState`.
 
         Print a warning if values in a state vector is out of the clipping range.
-        
+
         """
         if not(clipped_pos_xy == np.array(state[0:2])).all():
             print("[WARNING] it", self.step_counter, "in FlyThruGateAviary._clipAndNormalizeState(), clipped xy position [{:.2f} {:.2f}]".format(state[0], state[1]))
--- scripts/reward_function.py
+++ scripts/reward_function.py
@@ -1,58 +1,81 @@
 import numpy as np
 import math
 
-g1 = [0.0, 0.0, 0.0]  # gate1 centre
-g2 = [0.0, 0.0, 0.0]  # gate2 centre
-dp = 0.0   # distance to the gate plane
-dn = 0.0  # distance of the quadrotor to the gate normal
-dmax = 0.0  # specifies a threshold on the distance to the gate center in order to activate the safety reward
-wg = 0.0  # side length of the rectangular gate
-a = 0.0  # hyperparameter that trades off between progress maximization and risk minimization
-b = 0.0  # weight for penalty w
-p = [0.0, 0.0, 0.0]  # current position
-wt = [0.0, 0.0, 0.0]  # body rates
+g1 = np.array([0, 1, 5])  # gate1 centre
+g2 = np.array([0, 4, 5])  # gate2 centre
+# gm1 = np.array([[1, 2, 0], [1, 2, 0], [1, 2, ]])
+gm1 = np.identity(3)
+gm2 = np.identity(3)
+# gm2 = np.array([[1, 2, 5], [1, 6, 3], [8, 2, 3]])
+wt = np.array([1, 0, 0])  # body rates
+
+p = np.array([0, 2, 5])  # current position
+p_prev = np.array([0, 0, 0])  # previous position
+wg = 0.75  # side length of the rectangular gate
 
 
-def euclidean_distance(point1, point2):
-    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2 + (point1[2] - point2[2])**2)
+dmax = 2  # specifies a threshold on the distance to the gate center in order to activate the safety reward
+a = 2  # hyperparameter that trades off between progress maximization and risk minimization
+b = -0.5  # weight for penalty body rate
 
 
-def s(p):
+def dp(p, g, gm):
+    '''
+    distance to the gate plane
+    '''
+    return np.linalg.norm(np.dot(g - p, gm[:, 1]) / (np.linalg.norm(p - g)) * gm[:, 1])
+
+
+def dn(p, g, gm):
+    '''
+    distance of the quadrotor to the gate normal
+    '''
+    return np.linalg.norm(g - p - np.dot(g - p, gm[:, 1]) / (np.linalg.norm(p - g)) * gm[:, 1])
+
+
+def nearest_gate(p, g1, g2, gm1, gm2):
+    '''
+    Caluculates the nearest gate
+    '''
+    if np.linalg.norm(p - g1) <= np.linalg.norm(p - g2):
+        return g1, gm1
+    else:
+        return g2, gm2
+
+
+def collision_check(p):
+    return False
+
+
+def s(p, g1, g2):
     '''
     Defines the progress along the path segment that connects the previous gate center g1 with the next gate center g2.
+    p:position
     '''
-    return (p - g1) * (g2 - g1) / euclidean_distance(g1, g2)
+    # TODO: find previous gate and next gate
+    return np.dot(p - g1, g2 - g1) / np.linalg.norm(g1 - g2)
 
 
-def rp():
+def rp(p, p_prev, g1, g2):
     '''
     Progress Reward
+    Increases when the 
     '''
-    return s(p[t]) - s(p[t - 1])
+    return s(p, g1, g2) - s(p_prev, g1, g2)
 
 
-def rs():
+def rs(p, g, gm, dmax, wg):
     '''
-    Safety Reward
+    Safety Reward (only activated when drone is within threshold dmax) 
+    Value lies between 0 and -1.
+    increases when position is away from normal
     '''
-    f = max(1 - dp / dmax, 0.0)
+    f = max(1 - dp(p, g, gm) / dmax, 0.0)
     v = max((1 - f) * wg / 6, 0.05)
-    return -f**2 * (1 - math.exp(-0.5 * dn**2 / v))
+    return -f**2 * (1 - np.exp(-0.5 * dn(p, g, gm)**2 / v))
 
 
-def collision_check(p,gate):
-
-    return [False,p_crash,gate]
-
-
-def wb():
-    '''
-    Body rate
-    '''
-    return [0.0, 0.0, 0.0]
-
-
-def r():
+def final_reward(p, p_prev, g1, g2, gm1, gm2, a, b, dmax, wt, wg):
     '''
     Final Reward
     p_crash position of crash
@@ -60,11 +83,17 @@ def r():
     wt=body rates
     '''
 
-    wt_mag = euclidean_distance(wt, 0)
-    reward = rp() + a * rs() - b * wt_mag
-    if collision_check():
-        dg = euclidean_distance(p_crash, g)
+    wt_mag = np.linalg.norm(wt)
+    g, gm = nearest_gate(p, g1, g2, gm1, gm2)
+    print("rp", rp(p, p_prev, g1, g2))
+    print("rs", rs(p, g, gm, dmax, wg))
+    reward = rp(p, p_prev, g1, g2) + a * rs(p, g, gm, dmax, wg) + b * wt_mag
+    if collision_check(p):
+        dg = np.linalg.norm(p - g)
         rt = -min((dg / wg)**2, 20.0)
         return reward + rt
     else:
         return reward
+
+
+print(final_reward(p, p_prev, g1, g2, gm1, gm2, a, b, dmax, wt, wg))
